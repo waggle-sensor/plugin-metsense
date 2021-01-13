d6e71d46692f8cbe988ef9a055ed63bc68c8b8d6
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-01-13 14:00:29 -0600

    added sage.json and README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Metsense Plugin
+
+This plugin works with the [BME280](https://www.bosch-sensortec.com/products/environmental-sensors/humidity-sensors-bme280/) sensor to provide basic meteorological data.
+
+Note: This plugin requires access to the I2C bus on the device and expects the BME280 to have address 0x76 (configurable).
--- main.py
+++ main.py
@@ -1,25 +1,47 @@
+import argparse
 from busio import I2C
 import adafruit_bme680
 import time
 import board
+import logging
+import waggle.plugin as plugin
 
 def main():
-    print("setup up i2c")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true", help="enable debug logs")
+    parser.add_argument("--address", default=0x76, type=int, help="i2c address to use")
+    parser.add_argument("--rate", default=3.0, type=float, help="sampling rate")
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO,
+                        format="%(asctime)s %(message)s",
+                        datefmt="%Y/%m/%d %H:%M:%S")
+
+    plugin.init()
+
+    logging.info("setting up i2c interface")
     i2c = I2C(board.SCL, board.SDA)
 
-    print("setup bme680")
-    bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, address=0x76)
+    logging.info("setting up BME280 sensor")
+    bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, address=args.address)
 
     # change this to match the location's pressure (hPa) at sea level
     bme680.sea_level_pressure = 1013.25
 
     while True:
-        print("\nTemperature: %0.1f C" % bme680.temperature)
-        print("Gas: %d ohm" % bme680.gas)
-        print("Humidity: %0.1f %%" % bme680.relative_humidity)
-        print("Pressure: %0.3f hPa" % bme680.pressure)
-        print("Altitude = %0.2f meters" % bme680.altitude)
-        time.sleep(2)
+        logging.info("Temperature %0.1f C, Gas %0.1f count, Humidity %0.1f %%RH, Pressure %0.1f hPa, Altitude %0.1f m",
+            bme680.temperature,
+            bme680.gas,
+            bme680.relative_humidity,
+            bme680.pressure,
+            bme680.altitude)
+
+        plugin.publish("env.temperature.bme280", bme680.temperature)
+        plugin.publish("env.relative_humidity.bme280", bme680.relative_humidity)
+        plugin.publish("env.pressure.bme280", bme680.pressure)
+        plugin.publish("env.altitude.bme280", bme680.altitude)
+
+        time.sleep(args.rate)
 
 if __name__ == "__main__":
     main()
