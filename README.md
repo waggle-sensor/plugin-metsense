57ef6df1e85c0fb7d7e1a42c32fdd5590fa58d95
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-01-13 14:04:34 -0600

    allow sea level to be configured

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This plugin works with the [BME280](https://www.bosch-sensortec.com/products/environmental-sensors/humidity-sensors-bme280/) sensor to provide basic meteorological data.
 
-Note: This plugin requires access to the I2C bus on the device and expects the BME280 to have address 0x76 (configurable).
+Note: This plugin requires access to the I2C bus on the device, expects the BME280 to have address 0x76 (configurable) and can / needs to have its sea level pressure at the node's location specified.
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
     parser.add_argument("--debug", action="store_true", help="enable debug logs")
     parser.add_argument("--address", default=0x76, type=int, help="i2c address to use")
     parser.add_argument("--rate", default=3.0, type=float, help="sampling rate")
+    parser.add_argument("--sea-level-pressure", default=1013.25, type=float, help="sea level pressure at sensor location")
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO,
@@ -26,7 +27,8 @@ def main():
     bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, address=args.address)
 
     # change this to match the location's pressure (hPa) at sea level
-    bme680.sea_level_pressure = 1013.25
+    logging.info("setting sea level pressure to %0.1f", args.sea_level_pressure)
+    bme680.sea_level_pressure = args.sea_level_pressure
 
     while True:
         logging.info("Temperature %0.1f C, Gas %0.1f count, Humidity %0.1f %%RH, Pressure %0.1f hPa, Altitude %0.1f m",
