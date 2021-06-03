348ed4404bfdd30bfe35961abb2e912059dfbc7e
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-06-03 14:28:53 -0500

    SAGE-867 converted metsense plugin to read from sys iio sensor

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,7 @@
 FROM python:3.8-alpine
 
-RUN apk add --no-cache i2c-tools
-
-# we pull in gcc and linux headers just long enough to compile a couple
-# python packages
 COPY requirements.txt .
-RUN apk add --no-cache gcc musl-dev && \
-    CFLAGS="-fcommon" pip3 install --no-cache-dir -r requirements.txt && \
-    apk del gcc musl-dev
+RUN pip3 install --no-cache-dir -r requirements.txt
 
 COPY . .
 
--- main.py
+++ main.py
@@ -1,49 +1,44 @@
 import argparse
-from busio import I2C
-import adafruit_bme680
+from pathlib import Path
 import time
-import board
 import logging
+import sys
 import waggle.plugin as plugin
+
+
+def find_device_path(name):
+    for path in Path("/sys/bus/iio/devices").glob("*/name"):
+        if path.read_text().strip() == name:
+            return path.parent
+    raise FileNotFoundError(f"unable to find path for device {name}")
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", action="store_true", help="enable debug logs")
-    parser.add_argument("--address", default=0x76, type=int, help="i2c address to use")
-    parser.add_argument("--rate", default=3.0, type=float, help="sampling rate")
-    parser.add_argument("--sea-level-pressure", default=1013.25, type=float, help="sea level pressure at sensor location")
+    parser.add_argument("--name", default="bme680", type=str, help="name of iio device to use")
+    parser.add_argument("--rate", default=30.0, type=float, help="sampling rate")
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO,
                         format="%(asctime)s %(message)s",
                         datefmt="%Y/%m/%d %H:%M:%S")
 
+    try:
+        device_path = find_device_path(args.name)
+    except FileNotFoundError:
+        logging.error("no iio device found for %s", args.name)
+        sys.exit(1)
+
     plugin.init()
 
-    logging.info("setting up i2c interface")
-    i2c = I2C(board.SCL, board.SDA)
-
-    logging.info("setting up BME680 sensor")
-    bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, address=args.address)
-
-    # change this to match the location's pressure (hPa) at sea level
-    logging.info("setting sea level pressure to %0.1f", args.sea_level_pressure)
-    bme680.sea_level_pressure = args.sea_level_pressure
+    meta = {"sensor": args.name}
 
     while True:
-        logging.info("Temperature %0.1f C, Gas %0.1f count, Humidity %0.1f %%RH, Pressure %0.1f hPa, Altitude %0.1f m",
-            bme680.temperature,
-            bme680.gas,
-            bme680.relative_humidity,
-            bme680.pressure,
-            bme680.altitude)
-
-        plugin.publish("env.temperature.bme680", bme680.temperature)
-        plugin.publish("env.relative_humidity.bme680", bme680.relative_humidity)
-        plugin.publish("env.pressure.bme680", bme680.pressure)
-        plugin.publish("env.altitude.bme680", bme680.altitude)
-
         time.sleep(args.rate)
+        for path in device_path.glob("in_*_input"):
+            key = path.name
+            value = float(path.read_text())
+            plugin.publish(f"iio.{key}", meta=meta)
 
 if __name__ == "__main__":
     main()
